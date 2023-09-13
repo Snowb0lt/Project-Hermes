d338ca74028076b8a9886b0f43f9c910eb535643
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-09-12 19:32:29 -0700

    Bounce pads added and bouncing the player

--- Assets/Scripts/Entities/BouncePad.cs
+++ Assets/Scripts/Entities/BouncePad.cs
@@ -8,15 +8,24 @@ public class BouncePad : MonoBehaviour, IObstacle
     [SerializeField] private Rigidbody2D playerRb;
     [SerializeField] private GameObject player;
 
+    //To Change the Speed of movement on the fly
+    [SerializeField] private int _moveSpeed;
+
     private void Awake()
     {
         padRb = GetComponent<Rigidbody2D>();
+        transform.position = GameObject.Find("Ground").transform.position + new Vector3(0, 2, 0);
     }
 
     private void Start()
     {
         player = GameObject.Find("Player");
         playerRb = player.gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        //transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
     }
 
     public void InteractWithPlayer()
