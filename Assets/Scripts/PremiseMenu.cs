d0822201551f3b1e002484500e8edcf7337fa386
Snowb0lt <129245958+Snowb0lt@users.noreply.github.com>
2023-10-04 12:09:27 -0700

    Added a game premise

---
+++ Assets/Scripts/PremiseMenu.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PremiseMenu : MonoBehaviour
+{
+    [SerializeField] List<GameObject> storytexts = new List<GameObject>();
+
+
+}
